90a14a96a17847bce071b2edcbe81723d2630155
Chase Derr <chasederr@Chases-iMac.local>
2019-01-08 22:42:22 -0700

    initial commit!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'weather-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'weather-app';
+  title = 'Weather App';
+
 }
